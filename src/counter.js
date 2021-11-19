fc6d7672039beeaef3fb2d34abd6c3130dd8e61a
solomirrors <solomirrors@icloud.com>
2021-11-19 10:14:10 +0100

    use React + Redux

---
+++ src/counter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Col, Row} from "react-bootstrap";
+
+const Counter = ({counter, inc, dec, rnd}) => {
+    return (
+        <div className='jumbotron'>
+            <Row className='d-flex align-items-center justify-content-center'>
+                <Col sm={6}>
+                    <h1 id="counter" className='justify-content-center' style={{fontSize: '256px', textAlign: 'center'}}>{counter}</h1>
+                </Col>
+                <Col sm={6} className='align-items-center justify-content-center w-100 h-100'>
+                    <button id="dec" className="btn btn-primary btn-lg m-4 j" style={{width: '150px', height: '50px'}} onClick={dec}>DEC</button>
+                    <button id="inc" className="btn btn-primary btn-lg m-4" style={{width: '150px', height: '50px'}} onClick={inc}>INC</button>
+                    <button id="rnd" className="btn btn-primary btn-lg m-4" style={{width: '150px', height: '50px'}} onClick={rnd}>RND</button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
+import React from "react";
+import ReactDOM from 'react-dom';
 import {createStore, bindActionCreators} from "redux";
 import reducer from "./reducer";
-import {inc, dec, rnd} from "./actions";
+import * as actions from "./actions";
+import Counter from "./counter";
 
 const initialState = 0;
 
 const store = createStore(reducer)
 const {dispatch} = store;
 
-const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
-    incDispatch: inc,
-    decDispatch: dec,
-    rndDispatch: rnd
-}, dispatch);
+const {inc, dec, rnd} =
+    bindActionCreators(actions, dispatch);
 
-document.getElementById('inc')
-    .addEventListener('click', incDispatch);
 
-document.getElementById('dec')
-    .addEventListener('click', decDispatch)
-
-document.getElementById('rnd')
-    .addEventListener('click', () => {
-        const payload = Math.floor(Math.random()*20);
-        rndDispatch(payload)
-    });
 
 const update = () => {
-    document.getElementById('counter')
-        .innerHTML = store.getState()
+    ReactDOM.render(<Counter
+        counter={store.getState()}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+            const value = Math.floor(Math.random()*20);
+            rnd(value);
+        }}
+    />, document.getElementById('root'));
 }
 
+update();
 store.subscribe(update)
