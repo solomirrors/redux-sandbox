801d0bd62ce9f5f2391364412543e1ddfaa4e661
solomirrors <solomirrors@icloud.com>
2021-11-19 09:44:35 +0100

    bindActionCreators and others

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore, bindActionCreators} from "redux";
+import reducer from "./reducer";
+import {inc, dec, rnd} from "./actions";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer)
+const {dispatch} = store;
+
+const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
+    incDispatch: inc,
+    decDispatch: dec,
+    rndDispatch: rnd
+}, dispatch);
+
+document.getElementById('inc')
+    .addEventListener('click', incDispatch);
+
+document.getElementById('dec')
+    .addEventListener('click', decDispatch)
+
+document.getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random()*20);
+        rndDispatch(payload)
+    });
+
+const update = () => {
+    document.getElementById('counter')
+        .innerHTML = store.getState()
+}
+
+store.subscribe(update)
---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+const reducer = (state = 0, action) => {
+    switch (action.type){
+        case 'inc':
+            return state+1;
+        case 'dec':
+            return state-1;
+        case 'rnd':
+            return state + action.payload;
+
+        default:
+            return state;
+    }
+    return 0;
+}
+
+export default reducer;
